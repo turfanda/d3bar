d6f43ade838b4c82996dd3f481babbb746f19fcd
Glitch (d3-barchart) <none>
2018-01-13 05:59:22 +0000

    🍖🌚 Checkpoint script.js:596315/14

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-
-      var svg = d3.select("svg");
+var d3;
+      
+var svg = d3.select("svg");
       var margin = 100;
       var width = svg.attr("width") - margin;
       var height = svg.attr("height") - margin;
@@ -11,7 +12,6 @@
       d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json",function(err,data){
         if(err)
           throw err;
-        asd = data;
         
         var minDate = new Date(data.data[0][0]);
         var maxDate = new Date(data.data[data.data.length-1][0]);
